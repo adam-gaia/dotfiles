315e548ba83d50271f03fc450168f0fe9cd25ce7
Adam Gaia <36975781+adam-gaia@users.noreply.github.com>
2020-02-28 12:44:23 -0700

    Added path to source-highlight utilities

--- OS/linux.sh
+++ OS/linux.sh
@@ -2,4 +2,6 @@
 # Linux-specific bash configuration
 
 # Rust cargo path for work
-export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$HOME/.cargo/bin:${PATH}"
+
+export PATH="/usr/share/source-highlight:${PATH}"

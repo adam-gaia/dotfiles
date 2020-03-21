8753b1570bbf017d2f3fd991d3ebe6d353b6dc2b
Adam Gaia <36975781+adam-gaia@users.noreply.github.com>
2020-03-21 16:51:08 -0600

    Added 'function' keyword to functions

--- bash/OS/mac.sh
+++ bash/OS/mac.sh
@@ -7,28 +7,30 @@ export BASH_SILENCE_DEPRECATION_WARNING=1
 # --------------------------------------------------------------------------------
 # Use Gnu utilities - Idea taken from https://github.com/denisidoro/dotfiles
 # --------------------------------------------------------------------------------
-dircolors() { gdircolors "$@"; }
-make() { gmake "$@"; }
-mv() { gmv "$@"; }
-sed() { gsed "$@"; }
-awk() { gawk "$@"; }
-find() { gfind "$@"; }
-grep() { ggrep "$@"; }
-head() { ghead "$@"; }
-mktemp() { gmktemp "$@"; }
-ls() { gls "$@"; }
-date() { gdate "$@"; }
-shred() { gshred "$@"; }
-cut() { gcut "$@"; }
-tr() { gtr "$@"; }
-od() { god "$@"; }
-cp() { gcp "$@"; }
-cat() { gcat "$@"; }
-sort() { gsort "$@"; }
-kill() { gkill "$@"; }
-xargs() { gxargs "$@"; }
-readlink() { greadlink "$@"; }
-export -f sed awk find head mktemp date shred cut tr od cp cat sort kill xargs readlink
+function dircolors() { gdircolors "$@"; }
+function make() { gmake "$@"; }
+function mv() { gmv "$@"; }
+function sed() { gsed "$@"; }
+function awk() { gawk "$@"; }
+function find() { gfind "$@"; }
+function grep() { ggrep "$@"; }
+function head() { ghead "$@"; }
+function mktemp() { gmktemp "$@"; }
+function ls() { gls "$@"; }
+function date() { gdate "$@"; }
+function shred() { gshred "$@"; }
+function cut() { gcut "$@"; }
+function tr() { gtr "$@"; }
+function od() { god "$@"; }
+function cp() { gcp "$@"; }
+function cat() { gcat "$@"; }
+function sort() { gsort "$@"; }
+function kill() { gkill "$@"; }
+function xargs() { gxargs "$@"; }
+function readlink() { greadlink "$@"; }
+
+# TODO: should I export these or not?
+# export -f sed awk find head mktemp date shred cut tr od cp cat sort kill xargs readlink
 
 
 # --------------------------------------------------------------------------------

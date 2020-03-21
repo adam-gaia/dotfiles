32b3e1b7d883b2b4e148c771ca88e3cfdb05c07d
Adam Gaia <36975781+adam-gaia@users.noreply.github.com>
2020-03-21 15:39:31 -0600

    Moved shell analytics source. Rearanged less color defines

--- bash/colors/color.sh
+++ bash/colors/color.sh
@@ -164,8 +164,10 @@ export LESS_TERMCAP_md=$'\e[1;34m'
 export LESS_TERMCAP_me=$'\e[0m'
 export LESS_TERMCAP_se=$'\e[0m'
 export LESS_TERMCAP_so=$'\e[01;33m'
-export LESS_TERMCAP_ue=$'\e[0m'
 export LESS_TERMCAP_us=$'\e[1;4;31m'
+export LESS_TERMCAP_ue=$'\e[0m'
+# Bug: exporting 'LESS_TERMCAP_us' last with 'set -x' makes all set -x output print red
+
 
 
 # --------------------------------------------------------------------------------

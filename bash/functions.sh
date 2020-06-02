39813bcedd96e0067c84827cb3c9312630d3661a
Adam Gaia <36975781+adam-gaia@users.noreply.github.com>
2020-06-02 10:47:12 -0600

    removed 'colors' function

--- bash/functions.sh
+++ bash/functions.sh
@@ -27,6 +27,7 @@ function reload()
     # TODO: can we first unsource/clear everything? (be careful when clearing env vars. Maybe don't clear them)
     #shellcheck source=./bash/bashrc
     source "${HOME}/.bashrc"
+    fixPathBloat
 }
 
 function reloadInputrc()
@@ -300,38 +301,7 @@ function uz()
 alias unzip='uz'
 alias extract='uz'
 
-function colors()
-{
-    # TODO: fix this color printing function
-    # Originally taken from https://codeberg.org/KubikPixel/dotfiles/src/branch/master/.bashrc
 
-    local fgc bgc vals seq0
-
-    printf "Color escapes are %s\n" '\e[${value};...;${value}m'
-    printf "Values 30..37 are \e[33mforeground colors\e[m\n"
-    printf "Values 40..47 are \e[43mbackground colors\e[m\n"
-    printf "Value  1 gives a  \e[1mbold-faced look\e[m\n\n"
-
-    # foreground colors
-    for fgc in {30..37}; do
-        # background colors
-        for bgc in {40..47}; do
-            fgc=${fgc#37} # white
-            bgc=${bgc#40} # black
-
-            vals="${fgc:+$fgc;}${bgc}"
-            vals=${vals%%;}
-
-            seq0="${vals:+\e[${vals}m}"
-            printf "  %-9s" "${seq0:-(default)} = '"
-            printf "${seq0}TEXT\e[m'"
-            echo ''
-            printf " \e[${vals:+${vals+$vals;}}1mBOLD\e[m"
-            echo ''
-        done
-        echo; echo
-    done
-}
 
 # --------------------------------------------------------------------------------
 # Math

4c1a120280954e6e4348f1ac2169a1a793f4a3e4
Adam Gaia <36975781+adam-gaia@users.noreply.github.com>
2020-08-03 21:23:11 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -103,7 +103,7 @@ deploy "${DOTFILEDIR}/gdbinit" "${HOME}/.gdbinit"
 export -f deploy
 export DOTFILEDIR
 export BACKUPDIR
-sudo su "deploy ${DOTFILEDIR}/gdbinit /root/.gdbinit" # TODO: on linux, might need 'su -c'
+sudo su root "deploy ${DOTFILEDIR}/gdbinit /root/.gdbinit" # TODO: on linux, might need 'su -c'
 
 
 

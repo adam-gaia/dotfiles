6a4f95935c78157368fa85ac640ed74a831ed388
Adam Gaia <36975781+adam-gaia@users.noreply.github.com>
2020-08-05 08:45:19 -0600

    Update color.sh

--- bash/colors/color.sh
+++ bash/colors/color.sh
@@ -32,7 +32,7 @@ export PS1="\n\[\033[01;32m\]\u\[\033[1m\]\[\033[00m\]@\[\033[01;32m\]\h\[\033[0
 # --------------------------------------------------------------------------------
 # Pywal theme
 # --------------------------------------------------------------------------------
-# Pywal cursor color may not show. Fix here: https://github.com/dylanaraps/pywal/issues/382
+# Pywal cursor color may not show. Fix here: https://github.com/dylanaraps/pywal/issues/382 # TODO Automate the fix with sed or something
 timeout 1 python3 -m pywal -q --theme "${DOTFILEDIR}/bash/colors/pywal_modified_monokai.json" --vte #TODO: save python location as a var, then sub here # TODO: fix highlighting issues from pywal on mac
 eval "$(dircolors "${DOTFILEDIR}/bash/colors/dircolors_Monokai")" # Associate .dircolors_Monokai file with the dircolors utility
 

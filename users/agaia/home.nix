acccd68192d6203f90dd36d00a2b2e93683c1b02
Adam Gaia <git@agaia.dev>
2022-08-16 21:11:47 +0000

    added alacritty

--- users/agaia/home.nix
+++ users/agaia/home.nix
@@ -258,17 +258,64 @@
     };
   }; 
 
-  programs.gnome-terminal = {
+  programs.alacritty = {
     enable = true;
-    profile = {
-      "5ddfe964-7ee6-4131-b449-26bdd97518f7" = {
-        default = true;
-	visibleName = "Adam Default";
-	audibleBell = false;
-      };
-    };
-  };
+    settings = {
+      dynamic_title: "true";
 
+      font = {
+        normal = {
+          family = "FiraCode Nerd Font";
+          style = "Regular";
+          #family: monospace
+          #style: Regular
+	};
+      
+
+        bold = {
+          family = "FiraCode Nerd Font";
+          style = "Bold";
+          # family: monospace
+          # style: Bold
+        };
+
+
+        italic = {
+          family = "FiraCode Nerd Font";
+          style = "Light";
+          # family: monospace
+          # style: Italic
+        };
+
+        bold_italic = {
+          family = "FiraCode Nerd Font";
+          style = "Regular";
+          # family: monospace
+          # style: Bold Italic
+        };
+
+      size = "14";
+
+      use_thin_strokes = "true";
+      };
+
+      draw_bold_text_with_bright_colors = "false";
+
+      cursor = {
+        blinking = "On";
+        vi_mode_style = "Beam";
+        blink_interval = "750";
+        unfocused_hollow = "true";
+        thickness = "0.15";
+      };
+
+      #key_bindings = {
+       # - { key: N, mods: Control|Shift, action: SpawnNewInstance }
+        # TODO: Because I set tmux as my login shell and then run zsh as a subshell,
+        # creating a new window will not open the with working directory the same as the previous window
+      # See https://github.com/alacritty/alacritty/issues/2155
+      #};
+  };
 
   programs.fzf = {
     enable = true;

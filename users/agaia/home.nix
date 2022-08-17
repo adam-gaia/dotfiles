e88733d1c0e0778408a99d0265c11d98bc598a2e
Adam Gaia <git@agaia.dev>
2022-08-17 17:54:57 +0000

    added color values to alacritty config

--- users/agaia/home.nix
+++ users/agaia/home.nix
@@ -314,7 +314,32 @@
         thickness = 0.15;
       };
 
-      colors = {};
+      colors = {
+        primary = {
+	  background = "#272822";
+	  foreground = "#F8F8F2";
+	};
+	normal = {
+          black = "#272822";
+	  red = "#F92672";
+	  green = "#A6E22E";
+	  yellow = "#F4BF75";
+	  blue = "#66D9EF";
+	  magenta = "#AE81FF";
+	  cyan = "#A1EFE4";
+	  white = "#F8F8F2";
+	};
+	bright = {
+          black = "#75715E";
+	  red = "#F92672";
+	  green = "#A6E22E";
+	  yellow = "#F4BF75";
+	  blue = "#66D9EF";
+	  magenta = "#AE81FF";
+	  cyan = "#A1EFE4";
+	  white = "#F9F8F5";
+	};
+      };
 
       key_bindings = [ 
 	{

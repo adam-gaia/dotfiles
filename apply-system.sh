0e849621ae61db8c3a3acb9f409c591d1543cf79
Adam Gaia <git@agaia.dev>
2022-08-17 12:19:06 +0000

    added root check

--- apply-system.sh
+++ apply-system.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -e
+
+if [[ "${EUID}" -ne 0 ]]; then
+	echo 'This script must be ran as root (use sudo).'
+        exit 1
+fi
+
 sudo nixos-rebuild switch --flake .#
 

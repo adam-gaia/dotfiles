efae6698f037624859e33444635875a13401b2ee
Adam Gaia <git@agaia.dev>
2022-12-06 14:01:05 -0700

    feat: add orion syncthing config as system service

--- modules/syncthing/orion.nix
+++ modules/syncthing/orion.nix
@@ -2,5 +2,31 @@
 {
   services.syncthing = {
     enable = true;
+    guiAddress = "localhost:8384";
+
+    datadir = "/home/agaia/Documents";
+    # TODO: the config dir must persist or this device will regen an ID. Find a way to do that better
+    configDir = "/home/agaia/.config/syncthing";
+
+    # Ignore settings set in the web UI - this file is the only source of truth
+    overrideDevice = true; 
+    overrideFolders = true;
+
+    devices = {
+      helix = { id = (builtins.readFile ./helix.id); };
+    };
+
+    # Take these folders on the host and put them 
+    folders = {
+      "Documents" = {
+        path = "/home/agaia/Documents";
+        ignorePerms = false;
+        devices = ["helix"];
+      };
+    };
   };
+
+  # Open ports for syncthing
+  networking.firewall.allowedTCPPorts = [ 8384 22000 ];
+  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
 }

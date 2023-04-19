713e2ddbc88d41f0bb9fdd7e2d23a259c0510676
Adam Gaia <git@agaia.dev>
2023-04-19 17:23:51 -0600

    fix: force loading vimwiki

--- dotfiles/astronvim/lua/user/plugins/user.lua
+++ dotfiles/astronvim/lua/user/plugins/user.lua
@@ -1,49 +1,60 @@
 return {
-    -- You can also add new plugins here as well:
-    -- Add plugins, the lazy syntax
-    -- "andweeb/presence.nvim",
-    -- {
-    --   "ray-x/lsp_signature.nvim",
-    --   event = "BufRead",
-    --   config = function()
-    --     require("lsp_signature").setup()
-    --   end,
-    -- },
-    {
-        "LnL7/vim-nix",
-    },
-    {
-        "tools-life/taskwiki",
-    },
-    {
-        -- Automatically save (n)vim sessions
-        -- Works with tmux resurrect (https://github.com/tmux-plugins/tmux-resurrect)
-        "tpope/vim-obsession",
-    },
-    {
-        "simrat39/inlay-hints.nvim",
-        config = function()
-            require("inlay-hints").setup({ eol = { right_alight = true } })
-        end,
-    },
-    {
-        "vimwiki/vimwiki",
-    },
-    {
-        -- Vim Monokai scheme
-        "crusoexia/vim-monokai",
-    },
-    {
-        -- Collection of Nvim monokai schemes
-        "sainnhe/sonokai",
-    },
-    {
-        "christoomey/vim-tmux-navigator",
-    },
-    {
-        "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-        config = function()
-            require("lsp_lines").setup()
-        end,
-    },
+	-- You can also add new plugins here as well:
+	-- Add plugins, the lazy syntax
+	-- "andweeb/presence.nvim",
+	-- {
+	--   "ray-x/lsp_signature.nvim",
+	--   event = "BufRead",
+	--   config = function()
+	--     require("lsp_signature").setup()
+	--   end,
+	-- },
+	{
+		"LnL7/vim-nix",
+	},
+	{
+		"tools-life/taskwiki",
+		lazy = false, -- Always load on start up
+	},
+	{
+		-- Automatically save (n)vim sessions
+		-- Works with tmux resurrect (https://github.com/tmux-plugins/tmux-resurrect)
+		"tpope/vim-obsession",
+	},
+	{
+		"simrat39/inlay-hints.nvim",
+		config = function()
+			require("inlay-hints").setup({ eol = { right_alight = true } })
+		end,
+	},
+	{
+		"vimwiki/vimwiki",
+		lazy = false, -- Always load on start up
+		init = function()
+			vim.g.vimwiki_list = {
+				{
+					path = "~/vimwiki/",
+					syntax = "markdown",
+					ext = ".md",
+				},
+			}
+		end,
+	},
+	{
+		-- Vim Monokai scheme
+		"crusoexia/vim-monokai",
+	},
+	{
+		-- Collection of Nvim monokai schemes
+		"sainnhe/sonokai",
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+	},
+	{
+		"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+		config = function()
+			require("lsp_lines").setup()
+		end,
+	},
 }
